b42fe0ce11bc5934aa44c43f767dd890e872f297
Algeneralo <obayda.alesawi@gmail.com>
2019-07-07 11:36:04 +0300

    create banner section

---
+++ public/assets/js/gallery-filter.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.filter-btn').on('click', function () {
+        console.log($(this).data('filter'));
+        $("#gallery-content").justifiedGallery({filter: $(this).data('filter')});
+    });
+    $("#gallery-content").justifiedGallery({
+        rowHeight: 300,
+        margins: 3,
+        randomize: true,
+        // lastRow:"center",
+    }).on('jg.complete', function () {
+        $('#gallery-content .photos').lightGallery({
+            selector: ".photos",
+            hash: true,
+            galleryId: 1
+        });
+        $('#gallery-content .design').lightGallery({
+            selector: ".design",
+            hash: true,
+            galleryId: 2
+        });
+        $('#gallery-content .video').lightGallery({
+            selector: ".video",
+            hash: true,
+            galleryId: 3
+        });
+    });
+})
---
+++ resources/views/website/articles/banner.blade.php
@@ -0,0 +1,11 @@
+<section class="banner" style="background: url({{'/assets/img/'.$image}}) no-repeat scroll center center;">
+    <div class="container">
+        <div class="banner-content">
+            <h4 class="text-center">{{$title}}</h4>
+            <div class="banner-link">
+                <a href="/">Home</a>
+                <a class="active" href="#">{{$name}}</a>
+            </div>
+        </div>
+    </div>
+</section>
