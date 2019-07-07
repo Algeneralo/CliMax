c14c8ecdc36590b50581b30d727b207012d68353
Algeneralo <obayda.alesawi@gmail.com>
2019-07-07 15:14:38 +0300

    add loader to website

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -3,10 +3,19 @@
 @includeIf('website.articles.head')
 
 <body>
+<div id="overlay">
+    <img src="{{asset('assets/images/website/loader.svg')}}" alt="Loading" /><br/>
+    Loading...
+</div>
 @includeIf('website.articles.header')
 @yield('body')
 @includeIf('website.articles.footer')
 </body>
 @includeIf('website.articles.footer_scripts')
+<script>
+    jQuery(window).ready(function(){
+        jQuery('#overlay').fadeOut();
+    });
+</script>
 @yield('script')
 </html>
