db12d1223f15b867637bd345145f3f8ef2839844
Algeneralo <obayda.alesawi@gmail.com>
2019-07-07 11:22:19 +0300

    add "home-link" hover color

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -16,6 +16,8 @@ $(document).on("scroll", function () {
 
             $(this).addClass('active');
             nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            if (nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active').length == 0)
+                nav.find('a[href="#slider"]').addClass('active');
         }
     });
 
@@ -32,39 +34,9 @@ $(window).on("load", function () {
         $("nav").addClass("shrink fixed-top");
 });
 
-//add overlay for video on home page
-var videoPlayButton,
-    videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-        renderVideoPlayButton: function () {
-            if (videoWrapper.contains(video)) {
-                this.formatVideoPlayButton()
-                video.classList.add('has-media-controls-hidden')
-                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-            }
-        },
 
-        formatVideoPlayButton: function () {
-            videoWrapper.insertAdjacentHTML('beforeend', '\
-                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-                    <circle cx="100" cy="100" r="75" fill="none" stroke-width="15" stroke="#fff"/>\
-                    <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
-                </svg>\
-            ')
-        },
 
-        hideVideoPlayButton: function () {
-            video.play()
-            videoPlayButton.classList.add('is-hidden')
-            video.classList.remove('has-media-controls-hidden')
-            video.setAttribute('controls', 'controls')
-        }
-    }
-
-videoMethods.renderVideoPlayButton()
-
+//scroll down animation
 $("a.nav-link").click(function (e) {
     //if it's not link,scroll down to section
     var navHeight = 0, scrollHieght = 0;
@@ -84,6 +56,6 @@ $("a.nav-link").click(function (e) {
         scrollHieght = $(href).offset().top - navHeight;
     }
 
-    $("html, body").animate({scrollTop: scrollHieght}, 500, 'swing', function () {
+    $("html, body").animate({scrollTop: scrollHieght}, 1500, 'swing', function () {
     });
 });
