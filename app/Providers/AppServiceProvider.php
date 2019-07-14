dd8e3739d0d3b4075bf0b5499b1a89fb23144bbf
Algeneralo <obayda.alesawi@gmail.com>
2019-07-14 10:56:01 +0300

    share general configuration to all views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Config;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (Schema::hasTable('config'))
+            View::share('config', Config::first());
     }
 }
